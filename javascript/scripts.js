0e233221830304c79eb22be92a3dc7631d833581
Abhijit Patil <157373320+AbhiFutureTech@users.noreply.github.com>
2024-08-30 22:43:43 +0530

    Update scripts.js

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,8 +1,4 @@
-/*!
-    * Start Bootstrap - Freelancer v6.0.5 (https://startbootstrap.com/theme/freelancer)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-freelancer/blob/master/LICENSE)
-    */
+
     (function($) {
     "use strict"; // Start of use strict
   
