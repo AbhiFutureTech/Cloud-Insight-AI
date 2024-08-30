6142ce472975788a1c0d7a5c028b70649d9d1412
Abhijit Patil <157373320+AbhiFutureTech@users.noreply.github.com>
2024-08-30 22:51:55 +0530

    Update aggregated_lambda_function.py

--- solution/aggregated_lambda_function.py
+++ solution/aggregated_lambda_function.py
@@ -1,18 +1,4 @@
 
-"""
-    Final AWS Lambda function skeleton. 
-    
-    Author: Explore Data Science Academy.
-    
-    Note:
-    ---------------------------------------------------------------------
-    The contents of this file should be added to a AWS  Lambda function 
-    created as part of the EDSA Cloud-Computing Predict. 
-    For further guidance around this process, see the README instruction 
-    file which sits at the root of this repo.
-    ---------------------------------------------------------------------
-
-"""
 
 # Lambda dependencies
 import boto3    # Python AWS SDK
